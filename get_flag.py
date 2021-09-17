2c8ee0bf3850d42c69ba691fdd202e2cd62cd9bb
VOiD-Ayush <86951368+VOiD-Ayush@users.noreply.github.com>
2021-09-17 21:47:56 +0530

    Add files via upload

---
+++ get_flag.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import os
+import subprocess
+
+# os.system("cp ./../starwars.zip .")
+
+while True :
+	output = subprocess.check_output("ls *.zip", shell=True)
+	output=str(output.decode("utf-8"))
+	print(output,"starwars.zip\n")
+	os.system("unzip {0}".format(output))
+	os.system("rm {0}".format(output))
+	if (output=="password.zip\n"):
+		os.system("mv .zip a.zip")
+	if (output=="flag.zip\n"):
+		print("\n\n\nHere's your flag : ",end="")
+		os.system("cat flag.txt")
+		exit(0)
+
+	
+

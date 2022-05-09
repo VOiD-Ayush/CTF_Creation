395e0774e434a033451e796a76e2753d8732a45d
VOiD-Ayush <86951368+VOiD-Ayush@users.noreply.github.com>
2022-05-09 09:42:09 -0400

    Update packer.py

--- packer.py
+++ packer.py
@@ -1,17 +1,17 @@
 #!/bin/python3
 
 import os
-r = open('./wd.txt','r').read()
-
-words=r.splitlines()
+import string    
+import random # define the random module  
 
 # os.system("zip flag.zip flag.txt")
 
 current_zip="flag.zip"
 
-for word in words:
-	os.system("zip {0}.zip {1}".format(word,current_zip))
+for i in range(1000):
+	ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = i%10))    
+	os.system("zip {0}.zip {1}".format(ran,current_zip))
 	os.system("rm {0}".format(current_zip))
-	current_zip="{0}.zip".format(word)
+	current_zip="{0}.zip".format(ran)
 
-# zip flag.zip flag.txt  
+# zip flag.zip flag.txt
